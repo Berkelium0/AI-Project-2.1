54281dfd4937cb26490911da80e98f17bc787a18
Bartu Sıvacı <bartusivaci@gmail.com>
2023-12-10 17:25:54 +0100

    Update main-bartu.py

--- main-bartu.py
+++ main-bartu.py
@@ -2,3 +2,8 @@ from pgmpy.models import BayesianNetwork
 from pgmpy.inference import VariableElimination
 from pgmpy.factors.discrete import TabularCPD
 from pgmpy.inference import CausalInference
+import daft
+
+model_inference = BayesianNetwork([("Country", "Parent"), ("Parent", "Child")])
+model_inference.to_daft(node_pos={"Country": (0, 0), "Parent": (2, 0), "Child": (1, 1)}).render()
+
