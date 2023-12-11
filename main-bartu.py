1b8e1911d2ab73d310ae8f00d7cc8d746289dbc3
Bartu Sıvacı <bartusivaci@gmail.com>
2023-12-11 21:09:45 +0100

    Update main-bartu.py

--- main-bartu.py
+++ main-bartu.py
@@ -1,9 +1,19 @@
-from pgmpy.models import BayesianNetwork
-from pgmpy.inference import VariableElimination
-from pgmpy.factors.discrete import TabularCPD
-from pgmpy.inference import CausalInference
-import daft
 import json
+from pgmpy.models import BayesianNetwork
+
+G = BayesianNetwork()
+
+def construct_family_tree(family_tree, target_person):
+    new_tree = {}
+    for item in family_tree:
+        new_tree[item["subject"]] = {"X": {"allele": "unknown", "probability": 0}, "Y": {"allele": "unknown", "probability": 0}}
+        subject_X = new_tree[item["subject"]]["X"]
+        subject_Y = new_tree[item["subject"]]["Y"]
+
+        item["subject"] --> item["object"]
+        item["subject"] --> subject_blood_type
+        item["object"] --> object_blood_type
+
 
 with open("problem-a-00.json", 'r') as fp:
     config = json.load(fp)
@@ -12,6 +22,4 @@ goal_person_name = config["queries"]["person"]
 tested_person_name = config["test-results"]["person"]
 tested_person_blood_type = config["test-results"]["result"]
 
-model_inference = BayesianNetwork([("Country", "Parent"), ("Parent", "Child")])
-model_inference.to_daft(node_pos={"Country": (0, 0), "Parent": (2, 0), "Child": (1, 1)}).render()
 
